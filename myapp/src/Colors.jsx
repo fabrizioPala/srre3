271b68b98f43a3a68ed74387c763437be0dc38af
fabrizioPala <148344967+fabrizioPala@users.noreply.github.com>
2024-01-30 17:48:50 +0100

    Create Colors.jsx

---
+++ myapp/src/Colors.jsx
@@ -0,0 +1,11 @@
+import { Color } from "./Color";
+export function Colors({ colorList}) {
+    
+  return (
+    <ul>
+      {colorList.map((color) => (
+        <Color key={color.id} color={color} />
+      ))}
+    </ul>
+  );
+}
